fc71647e0e6dd8302cc4f6aae5fddfd5b7aba979
sara-lopez <sara.lopez@petroprix.com>
2025-03-26 12:45:18 +0100

    feat:[MAINTABLE]:Crear clase para tabla principal con atributos

---
+++ src/main/java/com/sarawipay/practica/model/MainTable.java
@@ -0,0 +1,13 @@
+package com.sarawipay.practica.model;
+
+
+
+public class MainTable {
+    private String PK;
+    private String SK;
+    private long id; // long o String?
+    private String status;
+    private String gIndex2Pk;
+    private String createTime; // String o LocalDateTime?
+
+}
