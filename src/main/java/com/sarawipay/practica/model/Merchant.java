b258b74297c8f6f715d4f4d6d041414e29fa427c
sara-lopez <sara.lopez@petroprix.com>
2025-03-26 13:08:28 +0100

    feat: [MERCHANT]: Crear clase Merchant y enum MerchanType

---
+++ src/main/java/com/sarawipay/practica/model/Merchant.java
@@ -0,0 +1,7 @@
+package com.sarawipay.practica.model;
+
+public class Merchant {
+    private String name;
+    private String address;
+    private MerchantType merchantType;
+}
---
+++ src/main/java/com/sarawipay/practica/model/MerchantType.java
@@ -0,0 +1,6 @@
+package com.sarawipay.practica.model;
+
+public enum MerchantType {
+    MERCHANT_TYPE_PERSONAL_SERVICES,
+    MERCHANT_TYPE_FINANCIAL_SERVICES
+}
