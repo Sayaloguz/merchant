e307d45167943f9b7916e0a911dc77b0cea45c42
sara-lopez <sara.lopez@petroprix.com>
2025-03-27 12:15:10 +0100

    feat: [MERCHANT]: Actualizar modelo de datos

--- src/main/java/com/sarawipay/practica/model/Merchant.java
+++ src/main/java/com/sarawipay/practica/model/Merchant.java
@@ -1,7 +1,26 @@
 package com.sarawipay.practica.model;
 
-public class Merchant {
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+
+@DynamoDBTable(tableName = "MainTable")
+public class Merchant extends MainTable{
+
+    @DynamoDBAttribute(attributeName = "name")
     private String name;
+
+    @DynamoDBAttribute(attributeName = "address")
     private String address;
+
+    @DynamoDBAttribute(attributeName = "merchantType")
     private MerchantType merchantType;
+
+
 }
