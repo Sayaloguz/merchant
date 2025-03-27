e018752c68a9216018184506bd21d3202179bc53
sara-lopez <sara.lopez@petroprix.com>
2025-03-27 13:26:32 +0100

    build: [JAVACONFIG]: Añadir DynamoDBMapper

--- src/main/java/com/sarawipay/practica/config/JavaConfig.java
+++ src/main/java/com/sarawipay/practica/config/JavaConfig.java
@@ -1,7 +1,15 @@
 package com.sarawipay.practica.config;
 
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
 import com.sarawipay.practica.model.MainTable;
 import com.sarawipay.practica.model.Merchant;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,5 +23,16 @@ public class JavaConfig {
     @Bean
     public MainTable mainTable() {
         return new MainTable();
+    }
+
+    @Bean
+    public DynamoDBMapper dynamoDBMapper() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-east-1"))
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials("key", "key2")))
+                .build();
+        return new DynamoDBMapper(client);
     }
 }
