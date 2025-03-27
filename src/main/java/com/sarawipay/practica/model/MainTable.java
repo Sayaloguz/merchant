a4ef72292b134fdc36fa84962ff8f1cd37b5199b
sara-lopez <sara.lopez@petroprix.com>
2025-03-27 12:14:58 +0100

    feat: [MAINTABLE]: Actualizar modelo de datos

--- src/main/java/com/sarawipay/practica/model/MainTable.java
+++ src/main/java/com/sarawipay/practica/model/MainTable.java
@@ -1,13 +1,36 @@
 package com.sarawipay.practica.model;
 
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+
+
+@DynamoDBTable(tableName = "MainTable")
+// FUTUROS CAMBIOS: ¿Clase abstracta? Eliminar constructores si al final cambio.
 public class MainTable {
+    @DynamoDBHashKey(attributeName = "PK") // Referenciará a "name"
     private String PK;
+
+    @DynamoDBRangeKey(attributeName = "SK") // Referenciará a "email"
     private String SK;
-    private long id; // long o String?
+
+    @DynamoDBAttribute(attributeName = "id")
+    private String id;
+
+    @DynamoDBAttribute(attributeName = "status")
     private String status;
+
+    @DynamoDBAttribute(attributeName = "gIndex2Pk")
     private String gIndex2Pk;
+
+    @DynamoDBAttribute(attributeName = "createTime")
     private String createTime; // String o LocalDateTime?
 
 }
